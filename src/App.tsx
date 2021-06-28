52e0b2533fe5fa7aa07efaa0509c9b3fdc899525
Clare Sudbery <clareworking@claresudbery.co.uk>
2021-06-28 18:38:49 +0100

    First front end change

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,9 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <p>
+          OK, done that in Windows. Now what?
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
